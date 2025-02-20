824fe0e5eebc85613e05030be9ad1285ef26530b
ryanshz <92750669+ryanshz@users.noreply.github.com>
2025-02-20 14:56:33 -0500

    works for uppercase 1-26 now, ignores all nonletter characters - calling it for now, want to move on to other projects, will work on 26+ at a later date

--- caesarciper.rb
+++ caesarciper.rb
@@ -8,34 +8,45 @@ def caesarcipher(string, shift_value)
   encryption = string.chars
 
   for i in 0...encryption.length
+    #lower
     if /[a-z]/.match?(encryption[i])
       encryption[i] = encryption[i].ord + shift_value
-      if encryption[i] >= 97
-        if shift_value == 0
-          encryption[i] = encryption[i].chr
+      if shift_value == 0
+        encryption[i] = encryption[i].chr
+      else
+        if shift_value >= 26
+          # could not figure out over 26, will come back to this later
+          encryption[i] = (encryption[i]-(26*((shift_value.to_f/26).ceil))).chr
         else
-          if shift_value >= 26
-            p (26*((shift_value.to_f/26).ceil))
-            p encryption[i]-(26*((shift_value.to_f/26).ceil))
-            p "---"
-            encryption[i] = (encryption[i]-(26*((shift_value.to_f/26).ceil))).chr
-          else
-            encryption[i] = 97 <= encryption[i] && encryption[i] <= 122 ? encryption[i].chr : (encryption[i]-26).chr
-          end
+          encryption[i] = 97 <= encryption[i] && encryption[i] <= 122 ? encryption[i].chr : (encryption[i]-26).chr
         end
-        # for z, needs to go from 122 to 97
       end
     end
-   end
+
+    #upper
+    if /[A-Z]/.match?(encryption[i])
+      encryption[i] = encryption[i].ord + shift_value
+      if shift_value == 0
+        encryption[i] = encryption[i].chr
+      else
+        if shift_value >= 26
+          # could not figure out over 26, will come back to this later
+          encryption[i] = (encryption[i]-(26*((shift_value.to_f/26).ceil))).chr
+        else
+          encryption[i] = 65 <= encryption[i] && encryption[i] <= 90 ? encryption[i].chr : (encryption[i]-26).chr
+        end
+      end
+    end
+  end
 
   encryption = encryption.join
   p "encrypted string: " + encryption
   p "---"
 end
 
-#for i in 26...52
-caesarcipher("abcdefghijklmnopqrstuvwxyz", 25)
-#end
+for i in 0...26
+  caesarcipher("Testing 123, Testing 123!", i)
+end
 
 # encryption[i] = shift_value > 26 ? (encryption[i]-(26*((shift_value.to_f/26).ceil))).chr : (encryption[i]-(26)).chr
 
