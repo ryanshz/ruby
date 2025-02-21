a82d6a6bb97de46a3e985675ea135904349fc6f6
ryanshz <92750669+ryanshz@users.noreply.github.com>
2025-02-21 10:38:03 -0500

    finished substring project (i guess im way better with string s*** than stuff from last project lol)

--- substring.rb
+++ substring.rb
@@ -1,5 +1,7 @@
+#searches dictionary for given string, if found, adds to hash and returns said hash
 def substring(searched_word, dictionary)
     found_words = Hash.new(0)
+    searched_word = searched_word.downcase
     dictionary.each do |word|
         if searched_word.match?(word)
             # check if found_words has value above zero
@@ -16,6 +18,27 @@ def substring(searched_word, dictionary)
     p found_words
 end
 
+#searches dictionary for given phrase, if found, adds all instances of phrase to hash and returns said hash
+def subarray(searched_phrase, dictionary)
+    found_words = Hash.new(0)
+    searched_phrase = searched_phrase.downcase
+    # split phrase into array by spaces
+    split_phrase = searched_phrase.split(" ")
+    dictionary.each do |word|
+        # pick word in dictionary
+        # iterate through phrase array, for instance of word found, add 1 to counter
+        # once phrase finished, add counter to hash and go to next dictionary value
+        word_count = 0
+        split_phrase.each do |searched_word|
+            if searched_word.match?(word)
+                word_count += 1
+                found_words[word] = word_count
+            end
+        end
+    end
+    p found_words
+end
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substring("below", dictionary)
+subarray("Howdy partner, sit down! How's it going?", dictionary)
